02dcab16d70c85ecea832e536b016d083d3d4250
Benjamin Hoover <benjamin.hoover@ibm.com>
2021-01-26 09:13:48 -0500

    Fix python to 3.6

--- flyvec/core.py
+++ flyvec/core.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 from .tokenizer import GensimTokenizer
 from pathlib import Path
+from cached_property import cached_property
 from functools import cached_property, lru_cache
 from typing import *
 from fastcore.test import *
